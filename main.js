32de23faeed2ade3de0d3def9c70929caa3a92c0
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2021-02-08 18:37:00 +0100

    Cleaned up code and html structure, styled intro images and CTAlinks

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 let sidebar = document.querySelector(".sidebar");
 
 // Save 'scroll location' from the current session.
-let location = sessionStorage.getItem("sidebar-scroll");
+let scrollPos = sessionStorage.getItem("sidebar-scroll");
 
 // If the 'scroll location' is not equals to null, 
 // then parse the string argument from sidebar.scrollTop and 
 // return an integer BASED on the specified radix 
 // (the base in mathematical numeric systems) 
 // which is ten in the decimal system.
-if (top !== null) {
-  sidebar.scrollTop = parseInt(location, 10);
+if (scrollPos !== null) {
+  sidebar.scrollTop = parseInt(scrollPos, 10);
 }
 
 // The beforeunload event is activated when the window and documents resources
